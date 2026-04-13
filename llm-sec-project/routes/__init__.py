86fdcb2df6cc7a90850d155fd8ac10c49d491384
kilpatrickmusk-web <kilpatrickmusk@gmail.com>
2026-04-13 18:50:40 +0800

    Delete llm-sec-project directory

--- llm-sec-project/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .commanroute import register_routes
-from .apiroute import register_api_route
-
-
-def register_all_routes(app):
-    '''register all routes'''
-    register_routes(app)
-    register_api_route(app)
